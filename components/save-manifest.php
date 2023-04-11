720a43af5a6bc3d862b3374c8c0713e3a986aece
Fehmi Özüseven <hello@fozuse.com>
2023-04-11 23:21:17 +0300

    Delete save-manifest.php

--- components/save-manifest.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$name = $_GET["name"];
-	$manifest = $_GET["manifest"];
-	$manifest = json_decode($manifest);
-	$manifest = json_encode($manifest, JSON_PRETTY_PRINT);
-	file_put_contents($name . ".json", $manifest);
-	echo "saved";
-?>
